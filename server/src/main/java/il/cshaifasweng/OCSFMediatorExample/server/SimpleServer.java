bb57bb9511934334a7ca61b83cd69ab33e9da146
DESKTOP-H33M1II\Omer A <Omerartzi5@gmail.com>
2023-04-01 19:46:10 +0300

    Implemented all empty functions in SimpleServer as described in the pdf

--- server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/SimpleServer.java
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/SimpleServer.java
@@ -6,7 +6,9 @@ import il.cshaifasweng.OCSFMediatorExample.server.ocsf.ConnectionToClient;
 import il.cshaifasweng.OCSFMediatorExample.server.ocsf.SubscribedClient;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class SimpleServer extends AbstractServer {
 	private static ArrayList<SubscribedClient> SubscribersList = new ArrayList<>();
@@ -48,25 +50,41 @@ public class SimpleServer extends AbstractServer {
 				client.sendToClient(message);
 			}
 			else if(request.startsWith("send Submitters IDs")){
-				//add code here to send submitters IDs to client
+				//we got a message from client requesting the submitters' ID, so we will send back to client the submitters IDs'
+				message.setMessage("318155447, 302576269");
+				client.sendToClient(message);
 			}
 			else if (request.startsWith("send Submitters")){
-				//add code here to send submitters names to client
+				//we got a message from client requesting the submitters' names, so we will send back to client the submitters names'
+				message.setMessage("Omer Artzi, Edan Peled Donenfeld");
+				client.sendToClient(message);
 			}
 			else if (request.equals("what’s the time?")) {
-				//add code here to send the time to client
+				//we got a message from client requesting the time in HH:mm:ss format, so we will send back to client the time in that format
+				SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+				Date date = new Date();
+				message.setMessage(sdf.format(date.getTime()));
+				client.sendToClient(message);
 			}
 			else if (request.startsWith("multiply")){
-				//add code here to multiply 2 numbers received in the message and send result back to client
-				//(use substring method as shown above)
-				//message format: "multiply n*m"
-			}else{
-				//add code here to send received message to all clients.
-				//The string we received in the message is the message we will send back to all clients subscribed.
-				//Example:
-					// message received: "Good morning"
-					// message sent: "Good morning"
-				//see code for changing submitters IDs for help
+				//we got a message from client requesting to multiply 2 numbers, so we will send back to client the product
+				String[] numbers = request.substring(8).split("(?<=\\*)|(?=\\*)");
+				try {
+					if(!(numbers[1].equals("*")))
+					{
+						throw new Exception();
+					}
+					message.setMessage(Integer.toString((Integer.parseInt(numbers[0].trim()) * Integer.parseInt(numbers[2].trim()))));
+				}
+				catch (Exception e)
+				{
+					message.setMessage("Illegal Input");
+				}
+				client.sendToClient(message);
+							}else{
+				//we got a message from client we couldn't identify, so we will send back to all clients the message
+				message.setMessage(request);
+				sendToAllClients(message);
 			}
 		} catch (IOException e1) {
 			e1.printStackTrace();
